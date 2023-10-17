3bee2e6337cffc833660446390ec4d66980bd30a
Anna-Lazakovich <anna27659@gmail.com>
2023-10-17 22:41:46 +0300

    Добавление файлов

---
+++ task2/week10.js
@@ -0,0 +1,17 @@
+function showMessage() {
+	console.log('Я учу JavaScript!');
+}
+
+showMessage();
+
+
+
+function next() {
+    let catImage1 = document.getElementById('catImage1');
+    catImage1.scr = '../task2/assets/images/cat2.jpg';
+}
+
+function prev() {
+    let catImage1 = document.getElementById('catImage1');
+    catImage1.scr='../task2/assets/images/cat1.jpg';
+}
