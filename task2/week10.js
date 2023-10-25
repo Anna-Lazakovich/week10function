a608316569c2b74fac6be39b29d5511f75aa92e3
Anna-Lazakovich <anna27659@gmail.com>
2023-10-25 22:25:17 +0300

    fixed the link

--- task2/week10.js
+++ task2/week10.js
@@ -8,7 +8,7 @@ showMessage();
 
 function next() {
     let catImage1 = document.getElementById('catImage1');
-    catImage1.src = '../task2/assets/images/cat2.jpg';
+    catImage1.src = '../task2/assets/images/cat2.jpeg';
 }
 
 function prev() {
